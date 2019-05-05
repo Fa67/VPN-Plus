8a6d5b3bdaf0b36972f0f4cc5a83d4bc034395f3
Manh Pham <manh.pham@fujitechjsc.com>
2019-05-06 00:17:35 +0700

    Initial Commit

---
+++ VPN Plus/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VPN Plus
+//
+//  Created by Manh Pham on 5/6/19.
+//  Copyright © 2019 Manh Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
